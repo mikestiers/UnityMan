dad4d1fbc6efc49117ed79bbabcb23d308da1a06
Mike Stiers <mike.stiers@outlook.com>
2023-02-06 00:01:20 -0500

    synced up with last session

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
-
 public class PlayerController : MonoBehaviour
 {
-    // Components
+    //Components
     Rigidbody2D rb;
     Animator anim;
     SpriteRenderer sr;
 
-    // Movement variable
+    //movement var
     public float speed;
     public float jumpForce;
 
-    // Ground checking
+    //groundcheck stuff
     public bool isGrounded;
     public Transform groundCheck;
     public LayerMask isGroundLayer;
     public float groundCheckRadius;
-    public bool isAttacking;
-    public bool isFiring;
 
     // Start is called before the first frame update
     void Start()
@@ -33,73 +30,70 @@ public class PlayerController : MonoBehaviour
         if (speed <= 0)
         {
             speed = 6.0f;
-            Debug.Log("Speed was set incorrectly, defaulting to " + speed.ToString());
+            Debug.Log("Speed was set incorrect, defaulting to " + speed.ToString());
         }
 
         if (jumpForce <= 0)
         {
             jumpForce = 300;
-            Debug.Log("jumpForce was set incorrectly, defaulting to " + jumpForce.ToString());
+            Debug.Log("Jump force was set incorrect, defaulting to " + jumpForce.ToString());
         }
 
         if (groundCheckRadius <= 0)
         {
             groundCheckRadius = 0.2f;
-            Debug.Log("groundCheckRadius was set incorrectly, defaulting to " + groundCheckRadius.ToString());
+            Debug.Log("Ground Check Radius was set incorrect, defaulting to " + groundCheckRadius.ToString());
         }
 
         if (!groundCheck)
         {
             groundCheck = GameObject.FindGameObjectWithTag("GroundCheck").transform;
-            Debug.Log("groundCheck not set, finding it manually");
+            Debug.Log("Ground Check not set, finding it manually!");
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        AnimatorClipInfo[] curPlayingClip = anim.GetCurrentAnimatorClipInfo(0);
         float hInput = Input.GetAxisRaw("Horizontal");
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
 
-        // Jumping
+        if (curPlayingClip.Length > 0)
+        {
+            if (Input.GetButtonDown("Fire1") && curPlayingClip[0].clip.name != "Fire")
+            {
+                anim.SetTrigger("fire");
+            }
+            else if (curPlayingClip[0].clip.name == "Fire")
+            {
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+                rb.velocity = moveDirection;
+            }
+        }
+
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.velocity = Vector2.zero;
             rb.AddForce(Vector2.up * jumpForce);
         }
 
-        // Walking
-        Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
-        rb.velocity = moveDirection;
+        if (!isGrounded && Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("JumpAttack");
+        }
+
 
         anim.SetFloat("hInput", Mathf.Abs(hInput));
         anim.SetBool("isGrounded", isGrounded);
-        Debug.Log(hInput);
 
-        // Grounded
-        if (isGrounded)
-        {
-            anim.SetBool("isAttacking", false);
-        }
-
-        // Fire
-        if (Input.GetButtonDown("Fire1"))
-        {
-            anim.SetBool("isFiring", true);
-        }
+        //check for flipped and create an algorithm to flip your character
         if (hInput != 0)
-        {
-            anim.SetBool("isFiring", false);
-        }
-
-
-        // Jump Attack
-        if (isGrounded == false && Input.GetButtonDown("Fire2"))
-        {
-            anim.SetBool("isAttacking", true);
-            rb.AddForce(Vector2.down * jumpForce);
-        }
+            sr.flipX = (hInput < 0);
     }
 }
